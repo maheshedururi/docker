33b2f69778cc2ebde8b272d8a8c279fbe7548314
Mahesh Edururi <mahesh.srvs286@gmail.com>
2019-03-25 16:38:37 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(Hi there);
+
+});
+
+app.listen(8080, () => {
+
+    console.log('listening on pot 8080');
+
+});
